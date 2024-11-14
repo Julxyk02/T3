d80f17c51f717f331125e8ae409a635f0dd7f0fc
Julio Quiñonez <Julxyk02>
2024-11-14 08:25:19 -0500

    commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int opcion = 0;
+do{
+    Console.WriteLine("--- Menú de opciones ---");
+    Console.WriteLine("1) Crear cita");
+    Console.WriteLine("2) Listar citas");
+    Console.WriteLine("3) Modificar univerdiad");
+    Console.WriteLine("4) Fin");
+    Console.Write("Selecciona una opción: ");
+    opcion = int.Parse(Console.ReadLine());
+    switch (opcion){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            Console.WriteLine("Fin del programa");
+            break;
+        default:
+            Console.WriteLine("opcion no valida, vuelva a intentar");
+            break;
+    }
+
+} while(opcion !=4);
